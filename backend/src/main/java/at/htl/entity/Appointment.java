dd86922bdf708c5d419267d001b0986129b10102
Marah Steigersdorfer <madschi.steigersdorfer@gmx.at>
2021-09-26 18:44:38 +0200

    Steigersdorfer - created Entity - added Appointment

---
+++ backend/src/main/java/at/htl/entity/Appointment.java
@@ -0,0 +1,78 @@
+package at.htl.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "APPOINTMENT")
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "hairdresser_id")
+    private Hairdresser hairdresser;
+
+    @Column(name = "date")
+    private String date;
+
+
+    public Appointment() {
+    }
+
+    public Appointment(Hairdresser hairdresser, Customer customer, String date) {
+        this.hairdresser = hairdresser;
+        this.customer = customer;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Hairdresser getHairdresser() {
+        return hairdresser;
+    }
+
+    public void setHairdresser(Hairdresser hairdresser) {
+        this.hairdresser = hairdresser;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%03d: %s %s, %s %s, %s",
+                id,
+                hairdresser.getLastName(),
+                hairdresser.getFirstName(),
+                customer.getLastName(),
+                customer.getFirstName(),
+                getDate());
+    }
+}
