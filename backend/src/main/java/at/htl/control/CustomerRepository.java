809263e11f4f626c6186e21d2594e736592bca6e
Marah Steigersdorfer <madschi.steigersdorfer@gmx.at>
2021-10-14 20:40:26 +0200

    Steigersdorfer - created & implemented CustomerRepository

---
+++ backend/src/main/java/at/htl/control/CustomerRepository.java
@@ -0,0 +1,30 @@
+package at.htl.control;
+
+import at.htl.entity.Customer;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@ApplicationScoped
+public class CustomerRepository implements PanacheRepository<Customer> {
+
+    @Transactional
+    public Customer save(Customer customer) {
+        return getEntityManager().merge(customer);
+    }
+
+    public Customer findByName(String firstName, String lastName){
+        return find("name", firstName, lastName).firstResult();
+    }
+
+    public Customer findById(long id){
+        return find("id", id).firstResult();
+    }
+
+    public List<Customer> findAllCustomer(){
+        var query = getEntityManager().createQuery("Select c from Customer c", Customer.class);
+        return query.getResultList();
+    }
+}
