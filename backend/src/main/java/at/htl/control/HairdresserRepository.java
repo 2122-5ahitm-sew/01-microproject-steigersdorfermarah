bc08eac31c213ae3a93c505f45ead30e27155295
Marah Steigersdorfer <madschi.steigersdorfer@gmx.at>
2021-10-14 20:40:52 +0200

    Steigersdorfer - created & implemented HairdresserRepository

---
+++ backend/src/main/java/at/htl/control/HairdresserRepository.java
@@ -0,0 +1,31 @@
+package at.htl.control;
+
+import at.htl.entity.Hairdresser;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@ApplicationScoped
+public class HairdresserRepository implements PanacheRepository<Hairdresser> {
+
+    @Transactional
+    public Hairdresser save(Hairdresser hairdresser) {
+        return getEntityManager().merge(hairdresser);
+    }
+
+    public Hairdresser findByName(String firstName, String lastName){
+        return find("name", firstName, lastName).firstResult();
+    }
+
+    public List<Hairdresser> findAllHairdresser(){
+        var query = getEntityManager().createQuery("Select h from Hairdresser h", Hairdresser.class);
+        return query.getResultList();
+    }
+
+    public Hairdresser findById(long id){
+        return find("id", id).firstResult();
+    }
+
+}
